4a65dfe028a834d3d1d390d778dfed8c702b6627
VeePeeK <veep.95@mail.ru>
2017-07-14 17:55:16 +0500

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -1,5 +1,5 @@
 SELECT DISTINCT game 
 FROM users
 WHERE level > 10 and id IN (SELECT user_id FROM payments 
-GROUP BY user_id
-HAVING SUM(amount)>100)
+                            GROUP BY user_id
+                            HAVING SUM(amount)>100)
