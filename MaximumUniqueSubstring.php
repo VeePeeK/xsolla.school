11a7d4edba84b227352a9caf98d6bc4a2b583cfb
VeePeeK <veep.95@mail.ru>
2017-07-15 07:50:21 +0500

    Add files via upload

--- MaximumUniqueSubstring.php
+++ MaximumUniqueSubstring.php
@@ -4,16 +4,34 @@ class MaximumUniqueSubstring
 {
     public function findMaximumUniqueSubstring($str) {
         $array = array();
+        $result = "";
         for ($i=0;$i<strlen($str);$i++) {
             $cur = $str[$i];
+            $key = array_search($cur,$array);
             //adding to array if it is not there
-            if (!in_array($cur,$array))
+            if ($key===False)
             {
                 array_push($array,$cur);
             }
+            //if symbol exists in array
+            else
+            {
+                //if new substring is bigger than previous
+                if (strlen($result)<count($array))
+                {
+                    $result=implode($array);
+                }
+                //remove everything before current symbol in array, including current symbol in array
+                $array = array_slice($array,$key+1);
+                //add symbol to the end of array
+                array_push($array,$cur);
+            }
         }
-        //convert array back to string
-        $result = implode($array);
+        //final check
+        if (strlen($result)<count($array))
+        {
+            $result=implode($array);
+        }
         return $result;
     }
 
